da9e5423c6eff85b89f3bc2cb0f7771f83cb59e3
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-09-08 10:00:03 -0500

    added static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/military'
+
+  get 'static_pages/web'
+
+  get 'static_pages/aug'
+
+  get 'static_pages/drone'
+
+  get 'static_pages/patent'
+
+  get 'static_pages/paper'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
