0b2107efc31e0b43f283e2d9203e9aad5823c0af
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-09-19 17:46:34 -0500

    getting closer on home page

--- app/assets/javascripts/flipcard.js
+++ app/assets/javascripts/flipcard.js
@@ -1,22 +1,34 @@
-$(document).ready(function() {
+var carousel = document.querySelector('.carousel');
+var container = carousel.querySelector('.carousel-container');
+var pagination = carousel.querySelector('.carousel-pagination');
+var bullets = [].slice.call(carousel.querySelectorAll('.carousel-bullet'));
+var totalItems = container.querySelectorAll('.carousel-item').length;
+var percent = (100 / totalItems);
+var currentIndex = 0;
 
-  var stack1 = $('#stack1');
+function next(){
+    slideTo(currentIndex + 1);
+}
 
-  stack1.removeClass('start');
+function prev(){
+    slideTo(currentIndex - 1);
+}
 
-  stack1.hammer().on('tap', function(event) {
-    stack1.addClass('hover');
-    event.stopPropagation();
-  });
+function slideTo(index){
+    index = index < 0 ? totalItems - 1 : index >= totalItems ? 0 : index;
+    container.style.WebkitTransform = container.style.transform = 'translate(-' + (index * percent) + '%, 0)';
+    bullets[currentIndex].classList.remove('active-bullet');
+    bullets[index].classList.add('active-bullet');
+    currentIndex = index;
+}
 
+bullets[currentIndex].classList.add('active-bullet');
+carousel.querySelector('.carousel-prev').addEventListener('click', prev, false);
+carousel.querySelector('.carousel-next').addEventListener('click', next, false);
 
-  $(document).hammer().on('tap', function(event) {
-    stack1.removeClass('hover');
-    $('.card').removeClass('hover');
-  });
-
-  $('.card').hammer().on('tap', function(event) {
-      $('.card').removeClass('hover');
-      $(this).addClass('hover');
-  });
-});
+pagination.addEventListener('click', function(e){
+    var index = bullets.indexOf(e.target);
+    if(index !== -1){
+        slideTo(index);
+    }
+}, false);
